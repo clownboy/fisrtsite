edd2b8ec5475dff4c7c9d72089259f7c7aa2aa99
clownboy <dk_dante@qq.com>
2019-09-05 11:48:56 +0800

    线上版本

--- website/wsgi.py
+++ website/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings.web')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','website.mysettings.web')
 
 application = get_wsgi_application()
